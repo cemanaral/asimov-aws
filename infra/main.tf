8494796e3d5abf7d0af04abaad44ffadca86c108
Cem Anaral <cemanaral@hotmail.com>
2023-03-30 00:34:16 +0300

    refactor: disable port 22 on ec2 machines

--- infra/main.tf
+++ infra/main.tf
@@ -55,12 +55,12 @@ resource "aws_security_group" "asimov-sg" {
         cidr_blocks     = ["0.0.0.0/0"]
     }
 
-    ingress {
-        from_port       = 22
-        to_port         = 22
-        protocol        = "tcp"
-        cidr_blocks     = ["0.0.0.0/0"]
-    }
+    #ingress {
+    #    from_port       = 22
+    #    to_port         = 22
+    #    protocol        = "tcp"
+    #    cidr_blocks     = ["0.0.0.0/0"]
+    #}
 
     # for ECR image pull
     ingress {
